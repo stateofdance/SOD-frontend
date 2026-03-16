514c0d4b173e81a81de63490e4b8c675c7e61045
jajames23 <jamiedelrosario321@gmail.com>
2026-03-17 03:46:16 +0800

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURl: 'https://sod-backend-522164650053.asia-southeast1.run.app',
+  apiURl: '',
   coachesImg: '',
   closingImg: '',
   introVideo: ''
