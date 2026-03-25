edd72e9f3d5253b72237abfb772a7cee726f6811
JohnLloydProg <johnlloydunida0@gmail.com>
2026-03-25 23:56:02 +0800

    Added sign up details

--- src/app/components/account/signup/signup.ts
+++ src/app/components/account/signup/signup.ts
@@ -22,27 +22,33 @@ export class Signup {
   first_name = new FormControl('');
   last_name = new FormControl('');
   email = new FormControl('');
+  number = new FormControl('');
+  birthdate = new FormControl<Date|null>(null);
   use = new FormControl(false);
   privacy = new FormControl(false);
 
   enable() {
-    if (!this.first_name || !this.last_name || !this.email || !this.use || !this.privacy || !this.password || !this.confirm_password) return false;
+    if (!this.first_name || !this.last_name || !this.email || !this.use || !this.privacy || !this.password || !this.confirm_password || !this.birthdate || !this.number) return false;
     if (this.first_name.value?.length == 0) return false;
     if (this.last_name.value?.length == 0) return false;
     if (!this.email.value?.includes('@')) return false;
+    if (!this.birthdate.value) return false;
+    if (this.number.value?.length == 0) return false;
     if (this.password.value() != this.confirm_password.value()) return false;
     return this.use.value && this.privacy.value;
   }
 
   signUp() {
     
-    if (this.email.value && this.first_name.value && this.last_name.value) {
+    if (this.email.value && this.first_name.value && this.last_name.value && this.birthdate.value && this.number.value) {
       let user:User = {
         email:this.email.value,
         first_name:this.first_name.value,
         last_name:this.last_name.value,
         password:this.password.value(),
-        username:this.first_name.value + ' ' + this.last_name.value
+        username:this.first_name.value + ' ' + this.last_name.value,
+        contact:this.number.value,
+        birthdate:this.birthdate.value
       };
 
       this.service.register(user).then(data => this.router.navigate(['', {outlets:{sidebar: 'login'}}]));
